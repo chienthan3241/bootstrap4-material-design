63865945071cc2c84e686b89ed90c5f9be53934f
Mahn Cuong Tran <tmchut@yahoo.com>
2018-07-16 17:43:33 +0200

    add magentacloud

---
+++ routes/bretonen.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const _ = require('lodash');
+const path = require('path');
+const dirTree = require('directory-tree');
+const tree = dirTree('./public/cloud', {extensions:/\.(pdf|docx|jpg|dxf|dwg)/});
+
+
+
+/* GET wir bretonen. */
+router.get('/', function(req, res, next) {
+	//console.log(path.resolve(rootdir));
+	console.log(tree.children[0]);
+	res.render('bretonen', { title: 'Wir Bretonen', dirArr: tree });
+	
+});
+
+/* GET wir bretonen. */
+router.get('/download/:file(*)', function(req, res) {
+	var file = req.params.file;
+	console.log(path.join('./',file));
+	res.download(path.join('./',file));
+});
+
+
+module.exports = router;
